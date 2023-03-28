431e9b6db5d4f24fc874258f7cdb324433857757
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-03-28 18:27:09 +0100

    Improve libcurl example code

--- examples/libcurl-example/main.cpp
+++ examples/libcurl-example/main.cpp
@@ -3,27 +3,35 @@
 #include <curl/curl.h>
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
+    // userp is the pointer passed to the function setting CURLOPT_WRITEDATA
+    // In this case, assume its an std::string*
     ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+
+    return size * nmemb; // Returns the number of bytes written   
 }
 
+#define URL "https://www.google.com"
+
 int main(void){
-    CURL *curl;
-    CURLcode res;
+    CURL *curl; // A pointer to the CURL object
+    CURLcode res; // A CURLcode object to store the result of curl operations
+    
+    curl = curl_easy_init();
+    if(!curl){ // Return an error if the CURL object could not be initialized
+        return 1;
+    }
+
     std::string readBuffer;
 
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
+    curl_easy_setopt(curl, CURLOPT_URL, URL);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
 
-        std::cout << readBuffer << std::endl;
-    }
     
     // ... HTMLHarvester code goes here ...
+    std::cout << readBuffer << std::endl;
     
     return 0;
 }
